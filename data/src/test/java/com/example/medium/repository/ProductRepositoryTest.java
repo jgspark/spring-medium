67697a1ef4949873292bf483ade2a00e3f5dc6f0
newbalancer <pnw123456@icloud.com>
2022-12-04 10:23:35 +0900

    build: data module add

--- data/src/test/java/com/example/medium/repository/ProductRepositoryTest.java
+++ data/src/test/java/com/example/medium/repository/ProductRepositoryTest.java
@@ -1,16 +1,13 @@
 package com.example.medium.repository;
 
 import com.example.medium.domain.Product;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @ExtendWith(SpringExtension.class)
